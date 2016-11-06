fe6ce3d0d49c7f396de47a6224d8a2f512ef98c3
Florent Vilmart <florentvilmart@me.com>
2016-11-06 14:21:53 -0500

    Initial Commit

---
+++ Hosyou/Hosyou.h
@@ -0,0 +1,19 @@
+//
+//  Hosyou.h
+//  Hosyou
+//
+//  Created by Florent Vilmart on 16-11-06.
+//  Copyright © 2016 hosyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hosyou.
+FOUNDATION_EXPORT double HosyouVersionNumber;
+
+//! Project version string for Hosyou.
+FOUNDATION_EXPORT const unsigned char HosyouVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hosyou/PublicHeader.h>
+
+
